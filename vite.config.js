d1e2d6565e42c6a4747a6aeaecf506561e6193ed
martinrodriguez-dotcom <martin.rodriguez@siipallets.com>
2026-01-27 08:45:50 -0300

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// Configuración principal de Vite con el plugin de React
+export default defineConfig({
+  plugins: [react()],
+})
