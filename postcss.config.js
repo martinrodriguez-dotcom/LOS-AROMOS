b92cc49c4b64b7afba8344e7dd89dcda16a52ca3
martinrodriguez-dotcom <martin.rodriguez@siipallets.com>
2026-01-27 09:29:33 -0300

    Create postcss.config.js

---
+++ postcss.config.js
@@ -0,0 +1,12 @@
+/**
+ * Archivo de configuración de PostCSS.
+ * Este archivo integra Tailwind CSS y Autoprefixer en el flujo de trabajo de Vite.
+ */
+export default {
+  plugins: {
+    // Motor de diseño de Tailwind CSS
+    tailwindcss: {},
+    // Plugin para añadir prefijos de compatibilidad de navegadores automáticamente
+    autoprefixer: {},
+  },
+}
