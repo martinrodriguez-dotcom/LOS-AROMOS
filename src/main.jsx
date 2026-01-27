cc39bc9d6026930e524cfb78eeeeafb42b595e2d
martinrodriguez-dotcom <martin.rodriguez@siipallets.com>
2026-01-27 09:12:45 -0300

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+/**
+ * Archivo: src/main.jsx
+ * Punto de entrada principal para la aplicación "Los Aromos".
+ */
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
